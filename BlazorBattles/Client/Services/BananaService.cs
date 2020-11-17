899bdd3bbda10db8208bad426618559a5baa2ed4
Roberta Beniusiene <RobertaBeniusiene@gmail.com>
2020-11-17 22:44:24 +0200

    BananasService implementation

---
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public class BananaService : IBananaService
+    {
+        public int Bananas { get; set; } = 1000;
+
+        public event Action OnChange;
+
+        public void AddBananas(int amount)
+        {
+            Bananas += amount;
+            BananasChanged();
+        }
+
+        public void EatBananas(int amount)
+        {
+            Bananas -= amount;
+            BananasChanged();
+        }
+
+        void BananasChanged() => OnChange.Invoke();
+    }
+}
---
+++ BlazorBattles/Client/Services/IBananaService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services
+{
+    public interface IBananaService
+    {
+        int Bananas { get; set; }
+        void EatBananas(int amount);
+        void AddBananas(int amount);
+
+        event Action OnChange;
+    }
+}
