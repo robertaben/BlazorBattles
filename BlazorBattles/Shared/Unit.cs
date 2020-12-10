cd31c267ca5979fedb940889119d14be0c9ab92a
Roberta <robertabeniusiene@gmail.com>
2020-12-10 22:29:48 +0200

    IUnitService implemented

---
+++ BlazorBattles/Shared/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBattles.Shared
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Attack { get; set; }
+        public int Defence { get; set; }
+        public int HitPoints { get; set; } = 100;
+        public int BananaCost { get; set; }
+
+    }
+}
