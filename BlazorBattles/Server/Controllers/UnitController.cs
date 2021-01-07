4dd7fa8fe0989178a8251e114307d72d6d1f35f3
Roberta <robertabeniusiene@gmail.com>
2021-01-07 23:44:13 +0200

    database sqlite and crud operations integrated for Units

---
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -0,0 +1,73 @@
+﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UnitController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUnits()
+        {
+            var units = await _context.Units.ToListAsync();
+            return Ok(units);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            await _context.Units.AddAsync(unit);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with given id doesn't exist.");
+            }
+
+            dbUnit.Title = unit.Title;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.Defence = unit.Defence;
+            dbUnit.BananaCost = unit.BananaCost;
+            dbUnit.HitPoints = unit.HitPoints;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(dbUnit);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            var dbUnit = await _context.Units.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with given id doesn't exist.");
+            }
+
+            _context.Units.Remove(dbUnit);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Units.ToListAsync());
+        }
+    }
+}
---
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using BlazorBattles.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorBattles.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Unit> Units { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        
+    }
+}
